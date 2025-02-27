cede6018fdcf88bafc4c02b562a662d5a45da757
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-02-27 14:08:11 +0100

    Driend or foe solved.

---
+++ 7kyu/frien/foe.py
@@ -0,0 +1,24 @@
+"""
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours!
+Otherwise, you can be sure he's not...
+
+Input = ["Ryan", "Kieran", "Jason", "Yous"]
+Output = ["Ryan", "Yous"]
+
+Input = ["Peter", "Stephen", "Joe"]
+Output = []
+"""
+
+users_input = ["Ryan", "Kieran", "Jason", "Yous", "1234"]
+
+def friend_or_foe(l):
+    friends = []
+    for i in l:
+        if len(i) == 4:
+            friends.append(i)
+    print(friends)
+    return friends
+
+friend_or_foe(users_input)
