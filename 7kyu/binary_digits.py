bebc04df58b0a02d9cb43015ae88b51560178304
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-02-27 14:36:13 +0100

    Tried to solve binery code in digits. Solved in part, return to it later.

---
+++ 7kyu/binary_digits.py
@@ -0,0 +1,25 @@
+"""
+Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+Examples:
+1	0001
+2	0010
+3	0011
+4	0100
+5	0101
+6	0110
+7	0111
+8	1000
+9	1001
+10	1010
+"""
+
+my_list = [1, 1, 1, 1]
+
+def converter(l):
+    return (l[0]*2**3)+(l[1]*2**2)+(l[2]*2**1)+(l[3]*2**0)
+
+result = converter(my_list)
+print(result)
