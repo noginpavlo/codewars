40846af6bfc36d1d0bd7a544832709a712a48171
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-02-27 13:51:50 +0100

    Solved Xcount even better.

--- 8kyu/countX.py
+++ 8kyu/countX.py
@@ -13,17 +13,10 @@ XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
 XO("zzoo") => false
 """
 
-ss = "xo"
+ss = "xoxxxxxx"
 
 def xo(s):
-    xs = 0
-    os = 0
-    for i in s:
-        if i.lower() == "x":
-            xs += 1
-        elif i.lower() == "o":
-            os += 1
-    return True if xs == os else False
+    return s.lower().count("x") == s.lower().count("o")
 
 result = xo(ss)
 print(result)
