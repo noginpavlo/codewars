1dee1033a88c172e800c7a60c2e89a4fafc58ddf
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-02-27 12:29:01 +0100

    Solved

---
+++ 8kyu/remove.py
@@ -0,0 +1,18 @@
+"""
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+"""
+
+array = ["Keep", "Remove", "Keep", "Remove", "Keep", "Keep", "Remove", "Keep", "Remove", "Keep"]
+
+def remove(a):
+    print(a)
+    for i in a:
+        if i == "Remove":
+            a.remove(i)
+    print(a)
+    return
+
+remove(array)
