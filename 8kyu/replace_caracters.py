ca628b99f5b86108374d276d86d7f45f18692f96
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-02-27 11:07:51 +0100

    Solved

---
+++ 8kyu/replace_caracters.py
@@ -0,0 +1,16 @@
+"""
+Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+S is misinterpreted as 5
+O is misinterpreted as 0
+I is misinterpreted as 1
+The test cases contain numbers only by mistake.
+"""
+
+input_str = "111111115555555%%%%%%%%000000000"
+
+def correct(string):
+    string = string.replace('1','I').replace('0','O').replace('5','S')
+    print(string)
+    return string
+
+correct(input_str)
