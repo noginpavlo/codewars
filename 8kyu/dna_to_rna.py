13632986d3524e3bb3698c8d07f611bcf0b7762e
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-02-27 10:57:55 +0100

    Better solution

--- 8kyu/dna_to_rna.py
+++ 8kyu/dna_to_rna.py
@@ -1,16 +1,9 @@
-dna_nucleotides = ["A", "C","G"]
-
-input_dna_seq = "acacacttttttttt"
+input_dna_seq = "ACACACACCACTTTTTTTTTT"
 
 """"Function assumes that the input is correct and only ACTG nucleotides are given."""
-def dna_converter(dna_seq):
-    rna_seq = ""
-    for n in input_dna_seq:
-       if n.upper() in dna_nucleotides:
-            rna_seq += n.upper()
-       else:
-           rna_seq += "U"
-    print(rna_seq)
-    return rna_seq
+def dna_converter(dna):
+    rna = dna.replace("T", "U")
+    print(rna)
+    return rna
 
 dna_converter(input_dna_seq)
