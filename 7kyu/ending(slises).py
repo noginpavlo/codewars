d9b0e5eb143b5ebf113e652f4203d543117e22ce
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-02-28 12:08:55 +0100

    Text ending slises problem solved.

---
+++ 7kyu/ending(slises).py
@@ -0,0 +1,20 @@
+"""
+Complete the solution so that it returns true
+ f the first argument(string) passed in ends with the 2nd argument (also a string).
+Examples:
+solution('abc', 'bc') # returns true
+solution('abc', 'd') # returns false
+"""
+
+txt = "abc"
+end = "bc"
+
+def solution(text, ending):
+    ending_len = len(ending)
+    if ending == text[-ending_len:]:
+        return True
+    else:
+        return False
+
+result = solution(txt, end)
+print(result)
