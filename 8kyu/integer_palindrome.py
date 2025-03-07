2ac712653965fe0b217b6b6917c56980d458206b
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-03-07 13:01:39 +0100

    Added extended palindrome problem

---
+++ 8kyu/integer_palindrome.py
@@ -0,0 +1,14 @@
+"""
+Given an integer, determine whether it is a palindrome.
+An integer is a palindrome if it reads the same forward and backward.
+
+Example:
+is_palindrome_number(121)  # should return True
+is_palindrome_number(-121)  # should return False
+is_palindrome_number(10)  # should return False
+"""
+
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+print(is_palindrome(121))
---
+++ 8kyu/test.py
@@ -0,0 +1,9 @@
+one_list = [1,1,1,1,1,2,23,3,4,5,7,7,8,9,0]
+
+one_set = set(one_list)
+
+print(one_set)
+
+new_list = [number for number in one_set]
+
+print(new_list)
