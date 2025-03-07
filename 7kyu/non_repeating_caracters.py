752996b8c2ae50b6884d2b16f277e259db95ed28
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-03-07 17:10:21 +0100

    Added unsolved problem with caracters that are not repeting

---
+++ 7kyu/non_repeating_caracters.py
@@ -0,0 +1,14 @@
+"""
+Write a function that returns the first non-repeating character in a given string. If all characters repeat, return None.
+
+Example:
+print(first_non_repeating("aabccdeff"))
+# Output: "b" (since "b" is the first character that appears only once)
+
+print(first_non_repeating("aabbcc"))
+# Output: None (since all characters repeat)
+"""
+
+
+def non_rep(s):
+    return None
