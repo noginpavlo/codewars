81d3c668000ed6c5825f2f540cad9e1c4079cb3f
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-03-07 11:00:25 +0100

    Added fizz buzz problem

---
+++ 7kyu/fizzbazz.py
@@ -0,0 +1,14 @@
+input = 15
+
+def fizz_buzz(n):
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("fizzbuzz")
+        elif i % 3 == 0:
+            print("fizz")
+        elif i % 5 == 0:
+            print("buzz")
+        else:
+            print(f"{i}")
+
+fizz_buzz(input)
