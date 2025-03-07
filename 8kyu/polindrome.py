41504b90f751ba99afe080dbc1c7f5ab88cce311
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-03-07 10:48:04 +0100

    Added polingrome probleb that is vary common and trycky

---
+++ 8kyu/polindrome.py
@@ -0,0 +1,15 @@
+"""
+Write a function that checks if a given string (case insensitive) is a palindrome.
+
+A palindrome is a word, number, phrase,
+or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar.
+"""
+
+string = "aaaaaaaaaaaaaa"
+
+def is_palindrome(s):
+    return s.lower() == s[::-1].lower()
+
+result = is_palindrome(string)
+print(result)
+
