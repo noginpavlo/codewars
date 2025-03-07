ab62c4e9dc71155e7b0ac99c516df0ae7cccd602
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-03-07 12:03:00 +0100

    Added Fibonacci problem solved

---
+++ 7kyu/fibonacci.py
@@ -0,0 +1,20 @@
+"""
+Write a function that returns the n-th Fibonacci number.
+The Fibonacci sequence starts with 0 and 1, and each subsequent number is the sum of the previous two.
+The sequence looks like this:
+0, 1, 1, 2, 3, 5, 8, 13, 21, ...
+
+Example:
+    fibonacci(5)  # should return 5 (0, 1, 1, 2, 3, 5)
+fibonacci(10) # should return 55
+"""
+
+
+def fibonacci(n):
+    numbers_list = [0, 1]
+    while len(numbers_list) <= n:
+        numbers_list.append(numbers_list[-1] + numbers_list[-2])
+    return numbers_list[-1]
+
+result = fibonacci(5)
+print(result)
