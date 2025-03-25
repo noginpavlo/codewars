f9a34fc2dd47c28cf0affdd01cba0247189545d2
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-03-25 15:39:51 +0100

    Now I understand and added better explaination through the code

--- code_snippets/dunder__methods/comparison__operators.py
+++ code_snippets/dunder__methods/comparison__operators.py
@@ -8,11 +8,12 @@ class Person:
 
     def __gt__(self, other):
         if isinstance(other, Person):
-            return f"I am older then you, {other.name}!"
+            return self.age > other.age
         else:
-            return f"It seems that you are older then me, {other.name}!"
+            return False
 
 p1 = Person("Josselin Beaumont", 42)
 p2 = Person("Pablo Escobar", 33)
 
 print(p2 > p1)
+print(p1 > p2)
