114ce48b972f66d4fdcf0d612e3e6f047e070ec5
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-03-28 14:39:07 +0100

    added great example of lambda function in combination with map()

--- code_snippets/wierd_syntax_I_cannot_remember/lambda.py
+++ code_snippets/wierd_syntax_I_cannot_remember/lambda.py
@@ -6,3 +6,6 @@ add = lambda a, b: a + b
 result2 = add(5, 5)
 print(f"Result 2: {result2}")
 
+my_list = [1,2,3,4,5]
+squared_list = map(lambda x: x ** 2, my_list)
+print(f"My list: {my_list};\nSquared list: {list(squared_list)}")
