9449986ce116448d21170cc21a190795c6eeb334
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-03-31 18:20:28 +0200

    added really cool example of lambda funciton usage

--- code_snippets/wierd_syntax_I_cannot_remember/lambda.py
+++ code_snippets/wierd_syntax_I_cannot_remember/lambda.py
@@ -6,6 +6,10 @@ add = lambda a, b: a + b
 result2 = add(5, 5)
 print(f"Result 2: {result2}")
 
-my_list = [1,2,3,4,5]
+my_list = [1,2,3,4,5,6,7,8,9,10]
 squared_list = map(lambda x: x ** 2, my_list)
 print(f"My list: {my_list};\nSquared list: {list(squared_list)}")
+
+
+even_list = list(filter(lambda x: x % 2 == 0, my_list))
+print(f"This is list sorted with lambda and filter functions: {even_list}")
