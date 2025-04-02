57cbdc40fa7a8bec2bde0dd744ba9692f4147757
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-04-02 16:07:56 +0200

    Here is more code

--- 6kyu/enumerate.py
+++ 6kyu/enumerate.py
@@ -5,9 +5,18 @@ or ")" if that character appears more than once in the original string.
 Ignore capitalization when determining if a character is a duplicate.
 """
 
+from collections import Counter
+
 def duplicate_encode(s):
-    count_items =
-    return "".join("(") if count_items[]
+    count_items = dict(Counter(s))
+    print(count_items)
+    result_string = ""
+    for count in count_items.values():
+        if count > 1:
+            result_string.join("more thebn oone ")
+        else:
+            result_string.join("!!!!")
+    return result_string
 
 result = duplicate_encode("aabcdd")
 print(result)
