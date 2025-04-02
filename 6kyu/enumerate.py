3be195a6866f059258aca9420d50d773fc07aae6
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-04-02 16:23:55 +0200

    Soved the problem by using Count() class

--- 6kyu/enumerate.py
+++ 6kyu/enumerate.py
@@ -3,6 +3,11 @@ The goal of this exercise is to convert a string to a new string where each char
 in the new string is "(" if that character appears only once in the original string,
 or ")" if that character appears more than once in the original string.
 Ignore capitalization when determining if a character is a duplicate.
+
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))(("
 """
 
 from collections import Counter
@@ -10,13 +15,13 @@ from collections import Counter
 def duplicate_encode(s):
     count_items = dict(Counter(s))
     print(count_items)
-    result_string = ""
-    for count in count_items.values():
-        if count > 1:
-            result_string.join("more thebn oone ")
+    result_string = "Here is a string: "
+    for letter in s:
+        if count_items[letter] == 1:
+            result_string += "("
         else:
-            result_string.join("!!!!")
+            result_string += ")"
     return result_string
 
-result = duplicate_encode("aabcdd")
+result = duplicate_encode("recede")
 print(result)
