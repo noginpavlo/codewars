33067cec7776460e71748ce71e4729225ee378a8
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-04-07 13:13:06 +0200

    solved

--- 6kyu/array.py
+++ 6kyu/array.py
@@ -12,7 +12,7 @@ If a = [1, 2, 2, 2, 3] and b = [2], the result should be [1, 3].
 
 
 def array_diff(l1, l2):
-    return []
+    return [item for item in l1 if item not in l2]
 
-result = array_diff([1,2], [1])
+result = array_diff([1,2,2,2,2,1,3,4,5], [1])
 print(result)
