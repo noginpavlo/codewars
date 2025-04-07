c748db4cc90ad0d10b9315769ec700760db79464
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-04-07 13:52:30 +0200

    Added good solution

--- 6kyu/is_panagram.py
+++ 6kyu/is_panagram.py
@@ -10,7 +10,7 @@ Return True if it is, False if not. Ignore numbers and punctuation.
 
 def is_panagram(st):
     alph = ["a", "b", "c", "d", "e", "f", "g", "h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    st_sorted = list(set([letter.lower() for letter in st if letter != " "]))
+    st_sorted = list(set([letter.lower() for letter in st if letter.isalpha()]))
     return True if sorted(st_sorted) == alph else False
 
 result = is_panagram("the quick brown fox jumps over the lazy dog")
