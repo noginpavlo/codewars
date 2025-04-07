17afefba69b5aa9e4ffb8e093db20fdb12e1fd5c
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-04-07 13:10:44 +0200

    added a problem to delete all the existing caracters in one list using other list

---
+++ 6kyu/array.py
@@ -0,0 +1,18 @@
+"""
+Implement a function that computes the difference between two lists.
+The function should remove all occurrences of elements from the first list (a)
+that are present in the second list (b).
+The order of elements in the first list should be preserved in the result.
+
+Example:
+If a = [1, 2] and b = [1], the result should be [2].
+
+If a = [1, 2, 2, 2, 3] and b = [2], the result should be [1, 3].
+"""
+
+
+def array_diff(l1, l2):
+    return []
+
+result = array_diff([1,2], [1])
+print(result)
