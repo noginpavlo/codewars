d79c75cac15e5cf6c9d755e865c0c9cbc61b295c
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-04-07 13:33:01 +0200

    bad soultion

--- 6kyu/is_panagram.py
+++ 6kyu/is_panagram.py
@@ -7,8 +7,11 @@ Given a string, detect whether or not it is a pangram.
 Return True if it is, False if not. Ignore numbers and punctuation.
 """
 
-def is_panagram(st):
-    return False
 
-result = is_panagram("")
+def is_panagram(st):
+    alph = ["a", "b", "c", "d", "e", "f", "g", "h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    st_sorted = list(set([letter.lower() for letter in st if letter != " "]))
+    return True if sorted(st_sorted) == alph else False
+
+result = is_panagram("the quick brown fox jumps over the lazy dog")
 print(result)
