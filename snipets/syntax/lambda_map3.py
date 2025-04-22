b341c74ee0fb92f06e272e4e590420a83cf19932
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-04-22 18:47:37 +0200

    one more problem with map and lambda

---
+++ snipets/syntax/lambda_map3.py
@@ -0,0 +1,13 @@
+"""
+### **Practice Problem 1: Using `lambda` and `map()`**
+Write a Python function to multiply each number in the list `[5, 10, 15, 20]`
+by 2 using a `lambda` inside the `map()` function.
+"""
+
+my_list = [5, 10, 15, 20]
+
+def multiplier(some_list):
+    return list(map(lambda x: x * 2, some_list))
+
+result = multiplier(my_list)
+print(result)
