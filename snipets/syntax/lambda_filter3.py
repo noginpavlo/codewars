9aaa83e6d14d588dce488ba15391d8c70fbf895d
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-04-22 18:56:59 +0200

    one more problem soleved with filter and lambda

---
+++ snipets/syntax/lambda_filter3.py
@@ -0,0 +1,19 @@
+"""
+### **Practice Problem: Using `lambda` and `filter()`**
+Write a Python function that filters out all the even numbers from a
+given list using a `lambda` inside the `filter()` function.
+
+Example Input:
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+Expected Output:
+    [1, 3, 5, 7, 9]
+"""
+
+n_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def get_evens(some_list):
+    return list(filter(lambda x: x % 2 == 0, some_list))
+
+result = get_evens(n_list)
+print(result)
