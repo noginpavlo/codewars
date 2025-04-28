afd83ce36859eb09796ec2e91cbe47d6ac4f292e
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-04-28 19:21:51 +0200

    Solved problem using generator expression inside max() function and learned what is the differense between this and list comprehension analogous use

---
+++ snipets/syntax/generator_expressions1.py
@@ -0,0 +1,24 @@
+"""
+Task:
+    You are given a list of strings.
+    Find the length of the longest word that starts with a vowel.
+
+Hint:
+    Use a generator expression inside the max() function.
+
+Example:
+    Input:
+        words = ["apple", "banana", "orange", "umbrella", "grape", "egg"]
+    Output:
+        8  # "umbrella"
+"""
+
+words = ["apple", "banana", "orange", "umbrella", "grape", "egg"]
+
+def longest(wrds):
+    return max(len(word) for word in wrds if word[0] in ["a", "e", "o", "u", "y"])
+
+
+result = longest(words)
+print(result) 
+
