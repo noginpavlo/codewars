3c9e05cbc907074f2c15a67aeb7d5911ee476a3b
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-05-09 20:36:16 +0200

    Rewised decorators by creating a decorator that measures execution time of function that just says hello

---
+++ snipets/syntax/decorators/measure_time_decorator.py
@@ -0,0 +1,22 @@
+import time
+from functools import wraps
+
+def measure_time(func):
+    """
+    Wrapper that measures the execution time of greet() function.
+    """
+    @wraps(func)  # This does not allow wraper to overwrite greet() metadata (like name for ex.)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"Executed in {end - start:.6f} seconds")
+        return result
+    return wrapper
+
+@measure_time
+def greet():
+    """Prints the last words that you'll hear before blackout."""
+    print("Hello, Josselin Beaumont!")
+
+greet()
