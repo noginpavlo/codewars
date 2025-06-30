768db5e8552ded291076502f34986b5e699ba0fc
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-06-30 12:14:40 +0200

    Solved 6kyu proglem about tower of starts

---
+++ 6kyu/tower.py
@@ -0,0 +1,38 @@
+"""
+Build a pyramid-shaped tower, as an array/list of strings,
+given a positive integer number of floors.
+A tower block is represented with "*" character.
+
+For example, a tower with 3 floors looks like this:
+
+[
+  "  *  ",
+  " *** ",
+  "*****"
+]
+
+And a tower with 6 floors looks like this:
+
+[
+  "     *     ",
+  "    ***    ",
+  "   *****   ",
+  "  *******  ",
+  " ********* ",
+  "***********"
+]
+"""
+
+
+def tower_builder(n_floors):
+    tower = []
+    for floor in range(n_floors):
+        stars = "*" * (2 * floor + 1)
+        spaces = " " * (n_floors - floor - 1)
+        tower.append(spaces + stars + spaces)
+    return tower
+
+
+the_tower = tower_builder(10)
+for each_tower in the_tower:
+    print(each_tower)
