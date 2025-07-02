27d5b247b734e91ae277624359b66ddfbafc6979
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-07-02 13:18:32 +0200

    Solved 6kyu kata about finding unique array element

---
+++ 6kyu/unique_n.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+"""
+There is an array with some numbers. All numbers are equal except for one.
+Try to find it!
+
+find_uniq([ 1, 1, 1, 2, 1, 1 ]) == 2
+find_uniq([ 0, 0, 0.55, 0, 0 ]) == 0.55
+
+It’s guaranteed that array contains at least 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.
+"""
+
+
+def find_uniq(arr):
+    for key, value in Counter(arr):
+        if value == 1:
+            return key
+
+
+result = find_uniq([1, 1, 1, 2, 1, 1])
+print(result)
