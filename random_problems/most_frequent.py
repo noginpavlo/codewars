d863257963ba0f9bd66b598e6316bc39d363f556
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-07-18 11:11:47 +0200

    Found most frequent element

---
+++ random_problems/most_frequent.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+"""
+Write a function that returns the most frequent element in a list.
+If there are multiple elements with the same highest frequency,
+return any one of them.
+"""
+
+my_list = [1, 3, 2, 1, 4, 1]  # expected 1
+
+
+def most_frequent(int_list: list[int]) -> int:
+    counter = Counter(int_list)
+    return max(counter, key=lambda x: counter[x])
+
+
+print(most_frequent(my_list))
