af54f28a21243014b03b6daee973353539e693b4
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-07-23 10:56:51 +0200

    Praciticed SRP SOLID by righting code

---
+++ theory/OOP/solid/srp.py
@@ -0,0 +1,56 @@
+"""
+SINGLE RESPONSIBILITY PRINCIPLE
+=============================================================================================
+
+is:
+=> A class should have only one reason to change, meaning it should have only one job or responsibility.
+"""
+
+# let's defind a BAD class that handles 3 things at a time
+class BadClass:
+    def __init__(self, data):
+        self.bad_data = data
+
+    def do_first_thing(self):
+        pass
+
+    def do_second_thing(self):
+        pass
+
+    def do_third_thing(self):
+        pass
+
+"""
+Such bad class as above invokes following problems potentially:
+1. Can couple functionality unintentionally.
+2. Makes it harder to test.
+3. Less readable code.
+"""
+
+
+# now let's create a classes that follow SRP
+class GoodClass:
+    def __init__(self, data):
+        self.good_data = data
+
+
+class DoFitstThing:
+
+    def do_the_thing(self, good_class): #  in this function good_class expects an instance of GoodClass to be passed as an argument
+        pass
+
+
+class DoSecontThing:
+
+    def do_the_thing(self, good_class):
+        pass
+
+
+class DoThirdThing:
+
+    def do_the_thing(self, good_class):
+        pass
+
+"""
+This code follows SRP
+"""
