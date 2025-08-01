217e41dd00ac4eaa098724ff2c9d4b3c11b77246
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-08-01 11:29:08 +0200

    Solve some problem with assignment operator usage.

---
+++ random_problems/assignment_expression/assingmentexp.py
@@ -0,0 +1,5 @@
+"""
+You are given an implicit list of integers from 1 to 30.
+You need to generate a list of square roots of numbers whose squares are greater than 500,
+but without calculating the square more than once per number.
+"""
