0588279f6d67c6d0bfdbe5b7ef14f3b10717c173
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-08-01 11:35:11 +0200

    Improved solution to warlus opperator problem.

--- random_problems/assignment_expression/assingmentexp.py
+++ random_problems/assignment_expression/assingmentexp.py
@@ -3,3 +3,13 @@ You are given an implicit list of integers from 1 to 30.
 You need to generate a list of square roots of numbers whose squares are greater than 500,
 but without calculating the square more than once per number.
 """
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 23, 30]
+
+
+def root(n_list: list[int]) -> list[int]:
+    return [num for n in n_list if (num := n * n) > 500]
+
+
+result = root(my_list)
+print(result)
