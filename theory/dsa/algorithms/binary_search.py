d61cb51400e699090ae1829adc969f888c6cae2b
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-08-06 11:25:01 +0200

    Added some theory to binary search file

--- theory/dsa/algorithms/binary_search.py
+++ theory/dsa/algorithms/binary_search.py
@@ -1,6 +1,41 @@
 """
 ->Binary search takes sorted list of elements as input. It needs to be sorted.
 ->It returns an index of the searched element or None.
+
+=> The idea of the algorithm is to choose a value exactly in the middle of the array and
+ask an conditional questtion about the searched value: is it bigger or smaller the the mid number?
+If bigger then the searched value is on the left,
+if smaller -- on the right.
+
+Initial List:
+Index:       0   1   2   3   4   5   6   7   8   9   10
+           [ 1 | 3 | 5 | 7 | 9 |11 |13 |15 |17 |19 |21 ]
+                          ↑
+                       mid=5 (11)
+                       13 > 11 → search right
+
+----------------------------------------------------------------
+
+Right Half:
+Index:              6   7   8   9   10
+                  [ 13 |15 |17 |19 |21 ]
+                            ↑
+                         mid=8 (17)
+                         13 < 17 → search left
+
+----------------------------------------------------------------
+
+Left of Right Half:
+Index:           6   7
+              [ 13 |15 ]
+                 ↑
+            mid=6 (13)
+      ✅ Found 13 at index 6!
+"""
+
+"""
+BS Big O complexity is log2(n),
+    where n is a length of an array.
 """
 
 my_list = [
@@ -125,3 +160,27 @@ def binary_search(int_list: list[int], searched_int: int) -> int | None:
 
 RESULT = binary_search(my_list, 1)
 print(RESULT)
+
+
+"""
+Question 1:
+
+Suppose you have a sorted list of 128 names, and you’re searching
+through it using binary search. What’s the maximum number of
+steps it would take?
+
+BS complexity is O(log2(n)),
+=> n_steps = log2(128) = 7
+
+RESPONSE: it takes 7 steps to search through 128 names.
+
+Question2:
+
+Suppose you double the size of the list. What’s the maximum
+number of steps now?
+
+not it is:
+n_steps = log2(256) = 8
+
+RESPONSE: 8 steps
+"""
