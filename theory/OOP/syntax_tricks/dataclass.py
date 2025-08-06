fb0eea81539c2f2484111d83e46102df43cd5ac4
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-08-06 12:02:03 +0200

    Expanded dataclass from pep 557 with explaining field(defauld_factory=list)

--- theory/OOP/syntax_tricks/dataclass.py
+++ theory/OOP/syntax_tricks/dataclass.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass, field
+from datetime import date
+
 """
 The basic boilerplate that we have when defining attributes is __init__.
 
@@ -14,19 +17,25 @@ The thing is that it is used just to avoid the huge boilerplate when a lot of at
 are defined in __init__.
 """
 
-from dataclasses import dataclass, field
-from datetime import date
+"""
+Data classes provide a more compact way of writing classes,
+without the boilerplate of having to set all variables with the same
+name in the __init__ method.
+"""
 
 
 # pylint says that there are too many instance attributes, and this id why we use @dataclass
 @dataclass
 class Book:
+    """This class defines a book and it has a lot of attributes to handle. Too many for __init__."""
+
     title: str
     author: str
     published: date
     pages: int
     price: float
     isbn: str | None = None
+    # fields(defalut_factory=list) is a great method that solves mutable default argument problem
     tags: list[str] = field(default_factory=list)
     in_stock: bool = True
 
