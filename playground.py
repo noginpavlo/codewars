d32ded8dfc340145cdd9467682609763158f30b3
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-10-24 18:01:59 +0200

    Implemented strategy in playground to practice inverview question

--- playground.py
+++ playground.py
@@ -1,8 +1,33 @@
-def infinite_recursion(n):
-    if n < 1:
-        return
-    print(n)
-    return infinite_recursion(n - 1)
+from abc import ABC, abstractmethod
 
 
-infinite_recursion(3)
+class Sender(ABC):
+    """Interface"""
+
+    @abstractmethod
+    def send(self, receiver: str, message: str) -> None: ...
+
+
+class EmailMessageSender(Sender):
+    """Concrete class that sends messages to receiver."""
+
+    def send(self, receiver: str, message: str) -> None:
+        print(f"Message {message} sent to {receiver}")
+
+
+class SenderStrategy:
+    """Orchestrator class that will be called by client."""
+
+    def __init__(self, sender: Sender) -> None:
+        self.sender = sender
+
+    def send_message(self, receiver: str, message: str) -> None:
+        self.sender.send(receiver, message)
+
+
+# client
+MY_RECEIRVER = "Pavlo"
+MY_MESSAGE = "Hey how's it going?"
+my_sender = EmailMessageSender()
+sndr = SenderStrategy(my_sender)
+sndr.send_message(MY_RECEIRVER, MY_MESSAGE)
