9f50b5e8290c4281b164ab9a5a0a181a9ce3cf54
TheApp Team <noreply@example.com>
2025-11-15 12:03:01 +0100

    feat(practice_refactoring) add problem to refactor the salery problem

--- playground.py
+++ playground.py
@@ -1,33 +1,4 @@
-from abc import ABC, abstractmethod
+my_list = [1,2,3,4,5,6,7]
 
-
-class Sender(ABC):
-    """Interface"""
-
-    @abstractmethod
-    def send(self, receiver: str, message: str) -> None: ...
-
-
-class EmailMessageSender(Sender):
-    """Concrete class that sends messages to receiver."""
-
-    def send(self, receiver: str, message: str) -> None:
-        print(f"Message {message} sent to {receiver}")
-
-
-class SenderStrategy:
-    """Orchestrator class that will be called by client."""
-
-    def __init__(self, sender: Sender) -> None:
-        self.sender = sender
-
-    def send_message(self, receiver: str, message: str) -> None:
-        self.sender.send(receiver, message)
-
-
-# client
-MY_RECEIRVER = "Pavlo"
-MY_MESSAGE = "Hey how's it going?"
-my_sender = EmailMessageSender()
-sndr = SenderStrategy(my_sender)
-sndr.send_message(MY_RECEIRVER, MY_MESSAGE)
+my_string = "".join(str(x) for x in my_list)
+print(my_string)
